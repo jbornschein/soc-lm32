00a590d445f8cea5e45ab1a80aa855c80ce5adc3
Joerg Bornschein <jb@capsec.org>
2007-11-09 12:26:18 +0100

    TestFirmware cleanup

---
+++ Software/TestFirmware/main.c
@@ -0,0 +1,92 @@
+#include "spike_hw.h"
+
+inline void writeint(uint32_t val)
+{
+	uint32_t i, digit;
+
+	for (i=0; i<8; i++) {
+		digit = (val & 0xf0000000) >> 28;
+		if (digit >= 0xA) 
+			uart_putchar('A'+digit-10);
+		else
+			uart_putchar('0'+digit);
+		val <<= 4;
+	}
+}
+
+void test2() {
+    uart_putchar('b');   
+}
+
+void test() {
+    uart_putchar('a');
+    test2();
+    uart_putchar('c');
+} 
+
+char glob[] = "Global";
+
+volatile uint32_t *p;
+volatile uint8_t *p2;
+
+int main()
+{
+    char test2[] = "Lokalerstr";
+    char *str = test2;
+    uint32_t i;
+    
+    for (i = 0; i < 4; i++)
+        test2[i] = 'l';
+    glob[0]  = 'g';
+    
+ 	// Initialize stuff
+	uart_init();
+	uart_putstr( "** Spike Test Firmware **\n" );
+
+	/* Test 1 */
+	uart_putstr( "Subroutine-Return Test: " );
+	test();
+	uart_putchar('\n');    
+
+	uart_putstr( "Local-Pointer Test:" );
+	for (;*str; str++) {
+	   uart_putchar(*str);
+	}
+	uart_putchar('\n');    
+	
+	uart_putstr( "Global-Pointer Test:" );
+	str = glob;
+	for (;*str; str++) {
+	   uart_putchar(*str);
+	}
+	uart_putchar('\n');    
+
+	uart_putstr( "Stack Pointer : " );
+	writeint(get_sp());
+	uart_putchar('\n');    
+
+	uart_putstr( "Global Pointer: " );
+	writeint(get_gp());
+	uart_putchar('\n');    
+
+	uart_putstr( "Memory Dump: " );
+	uint32_t *start = (uint32_t *)0x80000000;
+	uint32_t *end   = (uint32_t *)0x81000000;
+	uint32_t *p;
+	for (p=start; p<end; p++) {
+		if (((uint32_t)p & 12) == 0) {
+			uart_putstr("\r\n[");
+			writeint((uint32_t) p);
+			uart_putchar(']');    
+		}
+
+		uart_putchar(' ');    
+		writeint(*p);
+	}
+
+	uart_putstr("Entering Echo Test...\n");
+	while (1) {
+	   uart_putchar(uart_getchar());
+	}
+
+}
